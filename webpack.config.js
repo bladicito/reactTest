672bad23f2809c53769d3a7c1de9dd7a1ec7dc86
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-06-26 21:39:46 +0200

    css loaders added to web pack config file  as well as some dev dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     entry: [
         "./main.js"
@@ -20,11 +23,22 @@ module.exports = {
             {
                 test    : /\.less$/,
                 loader  : 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+                loader : 'url?prefix=font/&limit=10000'
             }
         ]
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
-    }
+    },
+    plugins : [new CopyWebpackPlugin([
+        { from: './assets/images/*.*', to: './public/' }
+    ])]
 
 };
